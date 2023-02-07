37587c6d2fb924e75d44258076a36fde633bc571
Daniel Ericsson <daniel.axel.ericsson@gmail.com>
2023-02-07 09:20:46 +0100

    Feat #12 Implemented BuildDao findAll method
    Method implemented and tested

--- backend/src/main/java/com/example/backend/daos/BuildDao.java
+++ backend/src/main/java/com/example/backend/daos/BuildDao.java
@@ -2,7 +2,11 @@ package com.example.backend.daos;
 
 import com.example.backend.models.Build;
 
+import java.util.List;
+
 public interface BuildDao {
 
     public Build insert(Build build);
+
+    public List<Build> findAll();
 }
