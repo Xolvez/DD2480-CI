a1e99fdec5ddde37e05c44aad5f9ea49828ed588
Daniel Ericsson <daniel.axel.ericsson@gmail.com>
2023-02-07 10:43:25 +0100

    Feat #7 Implement getBuilds endpoint
    Function for buildController implemented and tested

--- backend/src/main/java/com/example/backend/controller/BuildController.java
+++ backend/src/main/java/com/example/backend/controller/BuildController.java
@@ -5,10 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.models.Build;
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("build")
 public class BuildController {
     @Autowired
     public BuildService buildService;
+
+    @GetMapping("")
+    public List<Build> getBuilds() {
+        return buildService.findAll();
+    }
     
 }
