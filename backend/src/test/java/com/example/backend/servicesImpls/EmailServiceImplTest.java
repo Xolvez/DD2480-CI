5c2207e5a03a5b70cbd54cf17d289e5047171b02
Kawbat <ysiyang@126.com>
2023-02-08 10:03:54 +0100

    FEAT #34 EmailService interface and implementation
    Create EmailService interface and EmailServiceImpl, and write test for
    the implementation. add two dependencies to pom.xml

---
+++ backend/src/test/java/com/example/backend/servicesImpls/EmailServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.example.backend.servicesImpls;
+
+import com.example.backend.services.EmailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class EmailServiceImplTest {
+    @Autowired
+    private EmailServiceImpl emailService;
+    @Test
+    void test_email() {
+        String test_to_address = "ysiyang@126.com";
+        emailService.email_success("ysiyang@126.com");
+    }
+}
