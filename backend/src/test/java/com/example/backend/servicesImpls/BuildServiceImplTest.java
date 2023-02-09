e3193146942b190d5dec1065b96cc5d638f96e4b
Marcus Friberg <xolvez@gmail.com>
2023-02-09 09:05:56 +0100

    FEAT: Added properties for a test profile
    Added application properties for a test profile. To enable the
    properties, run with the "-Dspring.profiles.active=test" #59

--- backend/src/test/java/com/example/backend/servicesImpls/BuildServiceImplTest.java
+++ backend/src/test/java/com/example/backend/servicesImpls/BuildServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
